7dae9b1756f2a37719e503769457c6549d88f8d1
Tony Tran <codemonkey713@gmail.com>
2021-04-27 22:30:38 -0500

    added frame for navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
     <div className='App'>
+      <Navbar />
       <h1>Google Book Search</h1>
     </div>
   );
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <>
+      <div>Google Books</div>
+      <div>Search</div>
+      <div>Saved</div>
+    </>
+  );
+};
+
+export default Navbar;
