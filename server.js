792e78b6ecf91a91b86a6f9c7af87569672d5cae
Tony Tran <codemonkey713@gmail.com>
2021-05-02 17:52:42 -0500

    added mongoose connection server-side; updated scaffolding of server-side api and routes

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
+require('dotenv').config();
+
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 8080;
 const app = express();
+const routes = require('./routes');
+
+const mongoose = require('mongoose');
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(routes);
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
+
+// connect to our Mongo DB
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks',{
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
 // Send every other request to the React app
 // Define any API routes before this runs
