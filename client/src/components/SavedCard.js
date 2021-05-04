5282a0bac2b7c1b91f490590953a12d376573f20
Tony <codemonkey713@gmail.com>
2021-05-03 23:14:23 -0500

    added delete function to button on saved books

--- client/src/components/SavedCard.js
+++ client/src/components/SavedCard.js
@@ -36,6 +36,7 @@ const useStyles = makeStyles((theme) => ({
 export const SavedCard = (props) => {
   const classes = useStyles();
   const [books, setBooks] = useState([]);
+
   useEffect(() => {
     loadBooks();
   }, []);
@@ -48,7 +49,11 @@ export const SavedCard = (props) => {
   }
 
   function deleteBook(id) {
-    console.log('deleteBook function called with id: ', id);
+    // console.log('deleteBook function called with id: ', id);
+    API.deleteBook(id)
+      // add a modal to warn the book with 'title' has been deleted
+      .then((res) => loadBooks())
+      .catch((err) => console.log(err));
   }
 
   return (
@@ -56,49 +61,48 @@ export const SavedCard = (props) => {
       <Grid container spacing={3}>
         <Grid item xs={12}>
           <Paper className={classes.paper}>
-
             {/* begin mapping of all saved books from server */}
             {books.map((book) => (
               <Card className={classes.card}>
                 <CardActionArea>
                   <CardContent>
-                    <Typography gutterBottom variant="h5" component="h2">
+                    <Typography gutterBottom variant='h5' component='h2'>
                       {book.title}
                     </Typography>
                     <Typography
-                      variant="body2"
-                      color="textSecondary"
-                      component="p"
+                      variant='body2'
+                      color='textSecondary'
+                      component='p'
                     >
                       {book.authors}
                     </Typography>
                     <Typography
-                      variant="body2"
-                      color="textSecondary"
-                      component="p"
+                      variant='body2'
+                      color='textSecondary'
+                      component='p'
                     >
                       {book.description}
                     </Typography>
                     <CardMedia
                       className={classes.media}
                       image={book.image}
-                      title="API image"
+                      title='API image'
                     />
                   </CardContent>
                 </CardActionArea>
                 <CardActions>
                   <Button
                     href={book.link}
-                    target="_blank"
-                    size="small"
-                    color="primary"
+                    target='_blank'
+                    size='small'
+                    color='primary'
                   >
                     {props.btnView}
                   </Button>
                   <Button
                     onClick={() => deleteBook(book._id)}
-                    size="small"
-                    color="secondary"
+                    size='small'
+                    color='secondary'
                   >
                     {props.btnDelete}
                   </Button>
